73317e48a4d6aa9b2e9ad3e4b6a84fe9104f288e
vothin <zy757161350@qq.com>
2020-05-18 14:53:40 +0800

    连接数据库

--- common/link_mysql.py
+++ common/link_mysql.py
@@ -37,32 +37,37 @@ class LinkMysql():
     # 查询数据
     def insertData(self, name, value):
         sql = 'insert into care_carolie_item ' \
-              '(name, carolie, unit_qty, unit)' \
-              ' values(%s, 90, %s, "克")'
+              '(name, carolie, unit_qty, unit) ' \
+              'values(%s, 90, %s, "克")'
 
         self.cursor.execute(sql % (name, value))
         self.connect.commit()
         logs.info("成功插入数据")
+
         self.cursor.close()
         self.connect.close()
+        logs.info('关闭数据库')
 
 
     # 修改数据
     def deleteData(self):
-        sql = 'delete from care_carolie_item' \
-              'where bool_consume is Null'
+        sql = 'delete from care_carolie_item ' \
+              'where bool_consume is null'
 
         self.cursor.execute(sql)
         self.connect.commit()
         logs.info('成功删除数据')
+
         self.cursor.close()
         self.connect.close()
+        logs.info('关闭数据库')
 
 
 
 if __name__ == '__main__':
     l = LinkMysql('mysql_localhost')
-    l.insertData('1', 2)
+    # l.insertData('1', 2)
+    l.deleteData()
 
 
 
